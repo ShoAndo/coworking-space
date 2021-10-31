e5c95f95c78e91d00e041e204a83be71cf1e832f
Sho Ando <spdtas0223@gmail.com>
2021-10-31 13:39:41 +0900

    modelの作成

--- cowork/models.py
+++ cowork/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Store(models.Model):
+  address = models.CharField(max_length=256, blank=False)
+
+  def __str__(self):
+    return self.address
+
+class Seat(models.Model):
+  store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name="seats")
+  user = user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE, related_name="seat")
+  number = models.IntegerField(default=0, null=True, blank=True, verbose_name="number")
