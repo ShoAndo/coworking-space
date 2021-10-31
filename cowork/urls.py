099cbfe157417a7f89da4c39bb4351b0f49e7f3f
n2jjq <93418639+n2jjq@users.noreply.github.com>
2021-10-31 14:40:14 +0900

    ログアウト機能の追加

--- cowork/urls.py
+++ cowork/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # 一覧画面
     path('', views.index, name='index'),
     path('accounts/signup/', views.SignUpView.as_view(), name="signup"),
+    path('accounts/logout/', views.Logout.as_view(), name='logout'),
 ]
