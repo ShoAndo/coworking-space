8963644dd0641872d558ad1714dda70352e6f6b6
n2jjq <93418639+n2jjq@users.noreply.github.com>
2021-10-31 15:57:55 +0900

    reserve関数の追加

--- cowork/urls.py
+++ cowork/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/signup/', views.SignUpView.as_view(), name="signup"),
     path('accounts/logout/', views.Logout.as_view(), name='logout'),
+    path('reserve/<int:seat_id>/', views.reserve, name="reserve"),
 ]
--- cowork/views.py
+++ cowork/views.py
@@ -7,11 +7,20 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView ,LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .utils import get_defalt_store
+from .models import Seat
 
 
 def index(request):
   user = request.user
-  return render(request, 'index.html', {'user':user})
+  store = get_defalt_store()
+  seats = store.seats.all()
+  contents = {
+    'user': user,
+    'store': store,
+    'seats': seats
+  }
+  return render(request, 'index.html', contents)
 
 class SignUpView(generic.CreateView):
   form_class = UserCreationForm
@@ -22,3 +31,8 @@ class SignUpView(generic.CreateView):
 
 class Logout(LoginRequiredMixin, LogoutView):
   template_name = 'index.html'
+
+def reserve(request, seat_id):
+  seat = Seat.objects.get(id = seat_id)
+  print(seat)
+  return render(request, 'reserve.html')
